aba8a09d067d93823c1eee2c6647d5cf0bffb525
Tarek S Hafez <thafez@ymail.com>
2016-08-17 09:51:10 -0500

    Updated difference-of-squares/ to complete all tests. All tests green now to refactor.

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,15 +1,27 @@
 class Squares
-  attr_reader :square_of_sum, :sum_of_squares
+  attr_reader :square_of_sum, :sum_of_squares, :difference
 
   def initialize(n)
     @square_of_sum  = compute_square_of_sum(n)
     @sum_of_squares = compute_sum_of_squares(n)
+    @difference     = square_of_sum - sum_of_squarescd
   end
 
   def compute_square_of_sum(num)
     sum_to_num(num)**2
   end
 
+  def compute_sum_of_squares(num)
+    result = 0
+    (1..num).each do |i|
+      result += square_num(i)
+    end
+    result
+  end
+
+  def square_num(num)
+    num**2
+  end
   def sum_to_num(num)
     result = 0
     (1..num).each do |i|
